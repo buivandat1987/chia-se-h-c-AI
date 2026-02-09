2ebe23d69adef855a5397161acf7a0bf6af59a0f
buivandat1987 <giaoanlop4chantroi@gmail.com>
2026-02-09 16:19:06 +0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,7 @@
 
 import { GoogleGenAI, Type } from "@google/genai";
 
-const getAI = () => new GoogleGenAI({ apiKey: process.env.API_KEY || "" });
+const getAI = () => new GoogleGenAI({ apiKey: process.env.gemini_API_KEY || "" });
 
 export const generateSectionContent = async (
   sectionName: string,
